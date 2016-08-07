8d00a7e2bf6391452e8e0e511100ba7021c6fd21
zeplios <zhfchdev@gmail.com>
2016-08-07 22:57:21 +0800

    complete shape (resizing pending)

--- jquery.areaSelect.js
+++ jquery.areaSelect.js
@@ -8,6 +8,7 @@
 	console.log('jquery.areaSelect.js by Gongshw https://github.com/gongshw/jquery.areaSelect.js');
 
 	var AreaSelectStatus = {CREATE: 'create', MOVE: 'move', RESIZE: 'resize', NEAR: 'near'};
+	var AreaSelectType = {Rect: 'rect', CIRCLE: 'circle'};
 	var Direction = {
 		NE: {name: 'NE', x: 1, y: -1, cursor: 'nesw-resize'},
 		NW: {name: 'NW', x: -1, y: -1, cursor: 'nwse-resize'},
@@ -15,14 +16,26 @@
 		SW: {name: 'SW', x: -1, y: 1, cursor: 'nesw-resize'}
 	};
 	var DeleteMethod = {CLICK: 'click', DOUBLE_CLICK: 'doubleClick'};
+	var AreaSelectOption;
+
+	var areaMask;
 
 	function AreaSelect($ele, options) {
 		this.$ele = $ele;
 		this.init();
-		this.areas = options.initAreas;
+		this.areas = Array();
+		if (options.initAreas) {
+			for (var index in options.initAreas) {
+				var area = options.initAreas[index];
+				//this.areas.push(new RectArea(area.x, area.y, area.width, area.height));
+				this.areas.push(new EllipseArea(area.x+area.width/2, area.y+area.height/2, area.width/2, area.height/2, Math.PI / 4));
+			}
+		}
+		AreaSelectOption = options;
 		this.options = options;
 		this.status = AreaSelectStatus.CREATE;
 		this.fixRatio = options.fixRatio;
+		this.type = options.type;
 		this.dragging = false;
 		this.resizeDirection = null;
 		this.dragAreaOffset = {};
@@ -82,7 +95,8 @@
 				this.dragAreaOffset = {x: this.currentArea.x - x, y: this.currentArea.y - y};
 				break;
 			case AreaSelectStatus.CREATE:
-				var newArea = {x: x, y: y, width: 0, height: 0};
+				var newArea = new RectArea(x, y, 0, 0);
+				//var newArea = {x: x, y: y, width: 0, height: 0};
 				this.areas.push(newArea);
 				this.currentArea = newArea;
 				this.status = AreaSelectStatus.RESIZE;
@@ -118,7 +132,7 @@
 			this.currentArea = area;
 			var nearDrag = false;
 			var dragDirection = null;
-			var dragPoints = getPositionPoints(area);
+			var dragPoints = area.getPositionPoints();
 			for (var d in dragPoints) {
 				if (near({x: x, y: y}, dragPoints[d], this.options.padding)) {
 					nearDrag = true;
@@ -130,8 +144,7 @@
 				$canvas.css({cursor: dragDirection.cursor});
 				this.status = AreaSelectStatus.RESIZE;
 				this.resizeDirection = dragDirection;
-			}
-			else if (this.getArea(x, y, -this.options.padding) != undefined) {
+			} else if (this.getArea(x, y, -this.options.padding) != undefined) {
 				$canvas.css({cursor: 'move'});
 				this.status = AreaSelectStatus.MOVE;
 			} else {
@@ -197,21 +210,13 @@
 		g2d.lineWidth = this.options.area.lineWidth;
 		for (var index in this.areas) {
 			var area = this.areas[index];
-			this.g2d.strokeRect(area.x, area.y, area.width, area.height);
+			area.draw(g2d);
 		}
 		/* draw current area */
 		var area = this.currentArea;
 		g2d.fillStyle = this.options.point.fillStyle;
 		if (area != undefined) {
-			var positionPoints = getPositionPoints(area);
-			/* draw position point */
-			for (var index in positionPoints) {
-				var point = positionPoints[index];
-				g2d.beginPath();
-				g2d.arc(point.x, point.y, this.options.point.size, 0, Math.PI * 2, true);
-				g2d.closePath();
-				g2d.fill();
-			}
+			area.drawControlPoints(g2d);
 		}
 	};
 
@@ -227,23 +232,9 @@
 	};
 
 	AreaSelect.prototype.getArea = function (x, y, padding) {
-		padding = padding === undefined ? 0 : padding;
 		for (var index in this.areas) {
 			var area = this.areas[index];
-			var abs = Math.abs;
-			var x1 = area.x;
-			var x2 = area.x + area.width;
-			var y1 = area.y;
-			var y2 = area.y + area.height;
-			if (padding >= 0 && abs(x1 - x) + abs(x2 - x) - abs(area.width) <= padding * 2
-				&& abs(y1 - y) + abs(y2 - y) - abs(area.height) <= padding * 2) {
-				return area;
-			}
-			if (padding < 0
-				&& abs(x1 - x) + abs(x2 - x) - abs(area.width) == 0
-				&& abs(y1 - y) + abs(y2 - y) - abs(area.height) == 0
-				&& abs(abs(x1 - x) - abs(x2 - x)) <= abs(area.width) + 2 * padding
-				&& abs(abs(y1 - y) - abs(y2 - y)) <= abs(area.height) + 2 * padding) {
+			if (area.isInArea(x, y, padding)) {
 				return area;
 			}
 		}
@@ -268,7 +259,8 @@
 
 	var setAreaDirection = function (area, direction) {
 		if (area != undefined && direction != undefined) {
-			var x1 = area.x;
+			area.setAreaDirection(direction);
+			/*var x1 = area.x;
 			var x2 = area.x + area.width;
 			var y1 = area.y;
 			var y2 = area.y + area.height;
@@ -278,7 +270,7 @@
 			area.x = minOrMax[direction.x](x1, x2);
 			area.y = minOrMax[direction.y](y1, y2);
 			area.width = direction.x * width;
-			area.height = direction.y * height;
+			area.height = direction.y * height;*/
 		}
 	};
 
@@ -294,6 +286,182 @@
 		return event.offsetY ? event.offsetY : event.originalEvent.layerY;
 	};
 
+	function RectArea(x, y, width, height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	RectArea.prototype.draw = function(g2d) {
+		g2d.strokeRect(this.x, this.y, this.width, this.height);
+	}
+
+	RectArea.prototype.drawControlPoints = function(g2d) {
+		var positionPoints = this.getPositionPoints();
+		/* draw position point */
+		for (var index in positionPoints) {
+			var point = positionPoints[index];
+			g2d.beginPath();
+			g2d.arc(point.x, point.y, AreaSelectOption.point.size, 0, Math.PI * 2, true);
+			g2d.closePath();
+			g2d.fill();
+		}
+	}
+
+	RectArea.prototype.getPositionPoints = function () {
+		var points = {};
+		for (var d in Direction) {
+			points[d] = {
+				x: this.x + this.width * (Direction[d].x + 1) / 2,
+				y: this.y + this.height * (Direction[d].y + 1) / 2
+			};
+		}
+		return points;
+	};
+
+	RectArea.prototype.isInArea = function (x, y, padding) {
+		padding = padding === undefined ? 0 : padding;
+		var abs = Math.abs;
+		var x1 = this.x;
+		var x2 = this.x + this.width;
+		var y1 = this.y;
+		var y2 = this.y + this.height;
+		if (padding >= 0 && abs(x1 - x) + abs(x2 - x) - abs(this.width) <= padding * 2
+			&& abs(y1 - y) + abs(y2 - y) - abs(this.height) <= padding * 2) {
+			return true;
+		}
+		if (padding < 0
+			&& abs(x1 - x) + abs(x2 - x) - abs(this.width) == 0
+			&& abs(y1 - y) + abs(y2 - y) - abs(this.height) == 0
+			&& abs(abs(x1 - x) - abs(x2 - x)) <= abs(this.width) + 2 * padding
+			&& abs(abs(y1 - y) - abs(y2 - y)) <= abs(this.height) + 2 * padding) {
+			return true;
+		}
+		return false;
+	};
+
+	RectArea.prototype.setAreaDirection = function(direction) {
+		var x1 = this.x;
+		var x2 = this.x + this.width;
+		var y1 = this.y;
+		var y2 = this.y + this.height;
+		var width = Math.abs(this.width);
+		var height = Math.abs(this.height);
+		var minOrMax = {'1': Math.min, '-1': Math.max};
+		this.x = minOrMax[direction.x](x1, x2);
+		this.y = minOrMax[direction.y](y1, y2);
+		this.width = direction.x * width;
+		this.height = direction.y * height;
+	}
+
+	function EllipseArea(x, y, r1, r2, angle) {
+		this.x = x; // center x
+		this.y = y;
+		this.r1 = r1; // horizontal axis
+		this.r2 = r2;
+		this.angle = angle ? angle : 0;
+	}
+
+	EllipseArea.prototype.draw = function(g2d) {
+		var r = this.r1 > this.r2 ? this.r1 : this.r2;
+		var ratioX = this.r1 / r;
+		var ratioY = this.r2 / r;
+
+		var sin = Math.sin(this.angle);
+		var cos = Math.cos(this.angle);
+
+		/*g2d.beginPath();
+		g2d.setTransform(ratioX*cos, sin, -sin, ratioY*cos, this.x, this.y);
+		g2d.arc(0, 0, r, 0, 2 * Math.PI);
+		g2d.closePath(); 
+		g2d.stroke();*/
+
+		g2d.beginPath();
+		g2d.translate(this.x, this.y);
+		g2d.rotate(this.angle);
+		g2d.translate(-this.x, -this.y);
+		g2d.scale(ratioX, ratioY);
+		g2d.arc(this.x / ratioX, this.y / ratioY, r, 0, 2 * Math.PI);
+		g2d.closePath(); 
+		g2d.stroke();
+		g2d.scale(1 / ratioX, 1 / ratioY);
+		g2d.translate(this.x, this.y);
+		g2d.rotate(2*Math.PI - this.angle);
+		g2d.translate(-this.x, -this.y);
+		g2d.beginPath();
+		g2d.moveTo(this.x-this.r1*cos, this.y-this.r1*sin);
+		g2d.lineTo(this.x+this.r1*cos, this.y+this.r1*sin);
+		g2d.moveTo(this.x-this.r2*sin, this.y+this.r2*cos);
+		g2d.lineTo(this.x+this.r2*sin, this.y-this.r2*cos);
+		g2d.closePath(); 
+		g2d.stroke();
+	}
+
+	EllipseArea.prototype.drawControlPoints = function(g2d) {
+		var positionPoints = this.getPositionPoints();
+		/* draw position point */
+		for (var index in positionPoints) {
+			var point = positionPoints[index];
+			g2d.beginPath();
+			g2d.arc(point.x, point.y, AreaSelectOption.point.size, 0, Math.PI * 2, true);
+			g2d.closePath();
+			g2d.fill();
+		}
+	}
+
+	EllipseArea.prototype.getPositionPoints = function () {
+		var points = {};
+		var sin = Math.sin(this.angle);
+		var cos = Math.cos(this.angle);
+		points['NW'] = {x:this.x-this.r1*cos, y:this.y-this.r1*sin};
+		points['NE'] = {x:this.x+this.r1*cos, y:this.y+this.r1*sin};
+		points['SE'] = {x:this.x-this.r2*sin, y:this.y+this.r2*cos};
+		points['SW'] = {x:this.x+this.r2*sin, y:this.y-this.r2*cos};
+		return points;
+	};
+
+	EllipseArea.prototype.isInArea = function (x, y, padding) {
+		padding = padding === undefined ? 0 : padding;
+		var angle = Math.atan((this.y-y) / (this.x-x)) - this.angle;
+		while (angle >= Math.PI) angle -= Math.PI;
+		while (angle < 0) angle += Math.PI;
+		if (angle > Math.PI / 2) {
+			angle = Math.PI - angle;
+		}
+		var distance = (this.x - x) * (this.x - x) + (this.y - y) * (this.y - y);
+		var baseline = this.r1 * this.r1 * Math.cos(angle) * Math.cos(angle) + this.r2 * this.r2 * Math.sin(angle) * Math.sin(angle);
+		return distance < baseline;
+	};
+
+	EllipseArea.prototype.setAreaDirection = function(direction) {
+		/*var x1 = area.x;
+		var x2 = area.x + area.width;
+		var y1 = area.y;
+		var y2 = area.y + area.height;
+		var width = Math.abs(area.width);
+		var height = Math.abs(area.height);
+		var minOrMax = {'1': Math.min, '-1': Math.max};
+		area.x = minOrMax[direction.x](x1, x2);
+		area.y = minOrMax[direction.y](y1, y2);
+		area.width = direction.x * width;
+		area.height = direction.y * height;*/
+	}
+
+	EllipseArea.prototype.updateSize = function(x, y) {
+		/*var x1 = area.x;
+		var x2 = area.x + area.width;
+		var y1 = area.y;
+		var y2 = area.y + area.height;
+		var width = Math.abs(area.width);
+		var height = Math.abs(area.height);
+		var minOrMax = {'1': Math.min, '-1': Math.max};
+		area.x = minOrMax[direction.x](x1, x2);
+		area.y = minOrMax[direction.y](y1, y2);
+		area.width = direction.x * width;
+		area.height = direction.y * height;*/
+	}
+
 
 	$.fn.areaSelect = function (method) {
 		var as;
@@ -303,7 +471,8 @@
 			padding: 3,
 			area: {strokeStyle: 'red', lineWidth: 2},
 			point: {size: 3, fillStyle: 'black'}, 
-			fixRatio: false
+			fixRatio: false, 
+			type: 'rect'
 		};
 		as = this.data('AreaSelect');
 		if (as == undefined && (method === undefined || $.isPlainObject(method))) {
